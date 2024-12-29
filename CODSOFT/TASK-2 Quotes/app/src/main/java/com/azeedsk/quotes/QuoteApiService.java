70e08950c560c14cda015074d64794d39828fc94
azeed7860 <azeed2687@gmail.com>
2024-12-29 18:04:54 +0530

    -v

---
+++ CODSOFT/TASK-2 Quotes/app/src/main/java/com/azeedsk/quotes/QuoteApiService.java
@@ -0,0 +1,9 @@
+package com.azeedsk.quotes;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface QuoteApiService {
+    @GET("random")
+    Call<QuoteResponse> getRandomQuote();
+}
